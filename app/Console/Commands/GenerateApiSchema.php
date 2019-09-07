1095af620b901dc22cd7a4ba2f11d64020809a70
SeinopSys <seinopsys@gmail.com>
2019-09-07 19:57:42 +0200

    Deploy preparations

---
+++ app/Console/Commands/GenerateApiSchema.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+use function OpenApi\scan;
+
+class GenerateApiSchema extends Command {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'api:schema:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate the OpenAPI schema file';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle() {
+        $output_path = 'public/api.json';
+        $openapi = scan(app_path());
+        if (!$openapi->validate()){
+            $this->error("Invalid OpenAPI schema, could not generate $output_path");
+            exit(1);
+        }
+        Storage::disk('local')->put($output_path, $openapi->toJson());
+        $this->info('Written API schema to file');
+    }
+}
---
+++ setup/post-receive.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+echo "##### post-receive hook #####"
+read oldrev newrev refname
+echo "Push triggered update to revision $newrev ($refname)"
+
+RUN_FOR_REF="refs/heads/master"
+if [[ "$refname" ==  "$RUN_FOR_REF" ]]; then
+    GIT="env -i git"
+    CMD_CD="cd $(readlink -nf "$PWD/..")"
+    CMD_FETCH="$GIT fetch"
+    CMD_COMPOSER="sudo chmod -R ug+rw vendor/ && sudo -u www-data composer install --optimize-autoloader --no-dev 2>&1"
+    CMD_MIGRATE="sudo -u www-data php artisan migrate"
+    CMD_NPM="sudo -u www-data npm install --production --no-save"
+    # CMD_REDIS_CLEAR="# TODO"
+    CMD_API_DOCS="sudo -u www-data php artisan api:schema:generate"
+    CMD_CACHE_CONFIG="sudo -u www-data php artisan config:cache"
+    CMD_CACHE_ROUTES="sudo -u www-data php artisan route:cache"
+
+    echo "$ $CMD_CD"
+    eval ${CMD_CD}
+    echo "$ $CMD_FETCH"
+    eval ${CMD_FETCH}
+    echo "$ $CMD_COMPOSER"
+    eval ${CMD_COMPOSER}
+    echo "$ $CMD_MIGRATE"
+    eval ${CMD_MIGRATE}
+
+    if $GIT diff --name-only $oldrev $newrev | grep "^package-lock.json"; then
+        echo "$ $CMD_NPM"
+        eval $CMD_NPM
+    else
+        echo "# Skipping npm install, lockfile not modified"
+    fi
+
+    if $GIT diff --name-only $oldrev $newrev | grep "^assets/"; then
+        echo "$ $CMD_BUILD"
+        eval $CMD_BUILD
+    else
+        echo "# Skipping asset rebuild, no changes in assets folder"
+    fi
+
+    if $GIT diff --name-only $oldrev $newrev | grep "^app"; then
+        echo "$ $CMD_API_DOCS"
+        eval $CMD_API_DOCS
+    else
+        echo "# Skipping API schema generation, no changes in app folder"
+    fi
+
+    echo "$ $CMD_CACHE_CONFIG"
+    eval ${CMD_CACHE_CONFIG}
+
+    echo "$ $CMD_CACHE_ROUTES"
+    eval ${CMD_CACHE_ROUTES}
+
+    # echo "$ $CMD_REDIS_CLEAR"
+    # eval ${CMD_REDIS_CLEAR}
+else
+    echo "Ref does not match $RUN_FOR_REF, exiting."
+fi
+
+echo "##### end post-receive hook #####"
