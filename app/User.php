856016b9b8dd9b2fd8f7632a3c1047688f7fb5fb
SeinopSys <seinopsys@gmail.com>
2019-11-20 19:02:38 +0100

    Make new user fields fillable

--- app/User.php
+++ app/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'display_name', 'email', 'password',
+        'name', 'display_name', 'email', 'password', 'role', 'avatar_url'
     ];
 
     /**
