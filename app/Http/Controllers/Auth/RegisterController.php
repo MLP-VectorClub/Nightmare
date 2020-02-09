86e723a835b07ab8683ae3f92501bf8a61aeaac2
SeinopSys <david@seinopsys.dev>
2020-02-09 22:56:10 +0100

    Fix Auth facade import in RegisterController

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Rules\StrictEmail;
 use App\Rules\Username;
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
