d48bc346beac764faf60d9564832cb74f06e28d0
David Joseph Guzsik <seinopsys@protonmail.com>
2021-07-15 19:02:38 +0200

    New translations errors.php (Hungarian)

---
+++ resources/lang/hu/errors.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'auth' => [
+        'no_password_set' => 'You do not have a password set on the site, please sign in with the login method you used previously.',
+    ],
+    'color_guide' => [
+        'elastic_down' => 'The ElasticSearch server is unreachable',
+        'appearance_private' => 'This appearance is private and you do not have permission to view it',
+    ]
+];
